a708dfcbe4a60ff3c467d9c1a56f25c27c68bb29
lijie145 <lijie145@lu.com>
2016-03-21 09:37:26 +0800

    fundList

--- template/bbjj-20160225/js/index.js
+++ template/bbjj-20160225/js/index.js
@@ -17,7 +17,32 @@
             success: function (res) {
                 if (res.data) {
                     $.each(res.data, function (i, fund) {
-                        fund.fundData = fund[dataField];
+                        fund.fundData = fund[dataField] || '--';
+                        var discount, feeStyle, feeDiscount, fee, symbol;
+                        feeStyle = 'del';
+                        discount = feeStyle = feeDiscount = fee = '';
+                        if (!fund.extApplyProductFeeGson || !fund.extApplyProductFeeGson.fee) {
+                            discount = '';
+                            fee = feeDiscount = '--';
+                        } else {
+                            if(!fund.extApplyProductFeeGson.discount || fund.extApplyProductFeeGson.discount ==1) {
+                                discount = feeDiscount = feeStyle = '';
+                            } else if (fund.extApplyProductFeeGson.discount < 1) {
+                                discount = (fund.extApplyProductFeeGson.discount * 10).toFixed(2) + '折';
+                                feeDiscount = fund.extApplyProductFeeGson.discountFee.toFixed(2) + '%';
+                            } else if (fund.extApplyProductFeeGson.discount > 1) {
+                                discount = 1;
+                                feeDiscount = fund.extApplyProductFeeGson.discount + '元/笔';
+                            }
+                            if (fund.extApplyProductFeeGson.fee) {
+                                symbol = '%';
+                            }
+                            fee = fund.extApplyProductFeeGson.fee.toFixed(2) + symbol;
+                        }
+                        fund.feeDiscount = discount;
+                        fund.feeStyle = feeStyle;
+                        fund.discount = discount;
+                        fund.fee = fee;
                     });
 
                     var html = factory(model);
